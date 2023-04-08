d9b7c8f454224236f358d05f5bcf145458b40192
Sr-Carneiro <72666995+Sr-Carneiro@users.noreply.github.com>
2023-04-08 18:39:02 +0100

    Classes

--- leiloao/leiloaoapp/models.py
+++ leiloao/leiloaoapp/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+import datetime
+
+#AppUser: Representa um utilizador da aplicação
+class AppUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE())
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    #TODO: Caminho do image_path
+    image_path = models.CharField(max_length=255, default='.png')
+
+    def __str__(self):
+        return self.name
+
+# UserType: Representa um tipo de Utilizador(Admin, user, etc)
+class UserType(models.Model):
+    description = models.CharField(max_length=255)
+    def __str__(self):
+        return self.description
+
+class Sale(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    currentHighestBid = models.DecimalField()
+    # TODO: Caminho do image_path
+    image_path = models.CharField('Caminho da Imagem', max_length=255, default='.png')
+    isSold = models.BooleanField('Artigo Vendido')
+    initialAsk = models.DecimalField('Preço Inicial')
+    bidStartDate = models.DateTimeField('Data Publicação')
+    bidEndDate = models.DateTimeField('Data Publicação')
+    lastBidDate = models.DateTimeField('Data Publicação')
+    seller = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='Seller')
+    bidder = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='Bidder')
+
+class Bid(models.Model):
+    value = models.IntegerField('Valor')
+    bidder = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE, related_name='Sale')
+
+
+class WatchListLine(models.Model):
+    appUser = models.ForeignKey(AppUser, on_delete=models.CASCADE, related_name='AppUser')
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE, related_name='Sale')
+
